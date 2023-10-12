06a056707ec7dabe48ade2334b8c498c153c6efd
Ryan Pegg <ryan.luke.pegg@gmail.com>
2023-10-12 21:17:05 +0000

    [WIP] Installed RSpec
    * Installed RSpec
    * Installed FactoryBot
    * Installed ShouldaMatchers
    * Installed DatabaseCleaner
    * Created .env.example
    * Changed how the hosts are added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << 'juice.nitrosystems.co.uk'
+  # * Allow remote servers to be used during the development
+  config.hosts << ENV["DEV_HOST"] if ENV["DEV_HOST"].present?
 end
